c1fe9437805fb1bd123a98bdbe80b01a7b6299e9
Karol Wyliziński <k.wylizinski@wp.pl>
2016-10-20 14:39:48 +0200

    Add progress bars template without circle with live reloading

--- app/models/debate.rb
+++ app/models/debate.rb
@@ -1,4 +1,6 @@
 class Debate < ApplicationRecord
+  include ActionView::Helpers::TextHelper
+
   CODE_LENGTH = 5
 
   has_many :answers, dependent: :delete_all
@@ -10,6 +12,28 @@ class Debate < ApplicationRecord
 
   before_validation :set_code
   after_create :create_answers
+
+  def positive_count_with_person
+    votes_count = answers.where(answer_type: 1).first.votes.count
+    pluralize(votes_count, 'person')
+  end
+
+  def neutral_count
+    answers.where(answer_type: 2).first.votes.count
+  end
+
+  def negative_count_with_person
+    votes_count = answers.where(answer_type: 3).first.votes.count
+    pluralize(votes_count, 'person')
+  end
+
+  def positive_percent
+    "#{(answers.first.votes.count / votes.count.to_f * 100.0).round}%"
+  end
+
+  def negative_percent
+    "#{(answers.last.votes.count / votes.count.to_f * 100.0).round}%"
+  end
 
   private
 
