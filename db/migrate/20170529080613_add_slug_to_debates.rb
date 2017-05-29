df3ef3b08fec82dd279ffe3daf8016e4135f5823
pawurb <p.urbanek89@gmail.com>
2017-05-29 13:00:04 +0200

    remove incompatible migration code

--- db/migrate/20170529080613_add_slug_to_debates.rb
+++ db/migrate/20170529080613_add_slug_to_debates.rb
@@ -3,9 +3,4 @@ class AddSlugToDebates < ActiveRecord::Migration[5.0]
     add_column :debates, :slug, :string
     add_index :debates, :slug, unique: true
   end
-
-  Debate.find_each do |debate|
-    debate.generate_slug
-    debate.save!
-  end
 end
