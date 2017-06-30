a08bd08f705d28b02b0220977ed2457cab15e70b
Grzegorz Kemski <gregkem@gmail.com>
2017-06-30 18:36:15 +0200

    Add test and implement sending notification through notifier

--- app/services/debates/reset_service.rb
+++ app/services/debates/reset_service.rb
@@ -1,7 +1,18 @@
 module Debates
   class ResetService < DebateService
     def call
-      debate.votes.update_all answer_id: debate.neutral_answer.id
+      reset_votes
+      notify_about_reset
+    end
+
+    private
+
+    def reset_votes
+      debate.votes.update_all(answer_id: debate.neutral_answer.id)
+    end
+
+    def notify_about_reset
+      notifier.notify_about_reset(debate)
     end
   end
 end
--- spec/services/debates/reset_service_spec.rb
+++ spec/services/debates/reset_service_spec.rb
@@ -2,26 +2,33 @@ require 'rails_helper'
 
 describe Debates::ResetService do
   let(:debate)          { create(:debate) }
-  let(:positive_answer) { debate.answers.positive.take }
-  let(:neutral_answer)  { debate.answers.neutral.take }
-  let(:negative_answer) { debate.answers.negative.take }
+  let(:notifier)        { double(DebateNotifier) }
+  let(:positive_answer) { debate.positive_answer }
+  let(:neutral_answer)  { debate.neutral_answer }
+  let(:negative_answer) { debate.negative_answer }
   
-  subject { described_class.new(debate: debate) }
+  subject { described_class.new(debate: debate, notifier: notifier).call }
 
   before do
     create :vote, answer: positive_answer
     create :vote, answer: positive_answer
     create :vote, answer: neutral_answer
     create :vote, answer: negative_answer
+    allow(notifier).to receive(:notify_about_reset)
   end
 
   it 'changes all votes in the debate to neutral' do
-    expect { subject.call }.to change {
+    expect { subject }.to change {
       [positive_answer.votes.count, neutral_answer.votes.count, negative_answer.votes.count]
     }.from(
       [2, 1, 1]
     ).to(
       [0, 4, 0]
     )
+  end
+
+  it 'sends a notification' do
+    expect(notifier).to receive(:notify_about_reset).with(debate)
+    subject
   end
 end
