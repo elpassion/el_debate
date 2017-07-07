e56457e58b4d1f51f1ea4d231ce5e4ba721e943c
Grzegorz Kemski <gregkem@gmail.com>
2017-07-07 23:55:39 +0200

    Apply consistent indentation and line-breaking

--- spec/services/debate_notifier_spec.rb
+++ spec/services/debate_notifier_spec.rb
@@ -7,20 +7,17 @@ describe DebateNotifier do
   subject           { described_class.new(broadcaster: broadcaster) }
 
   it 'notifies about debate current state' do
-    expect(broadcaster)
-      .to receive(:push).with(channel, 'debate_changed', hash_including(:debate))
+    expect(broadcaster).to receive(:push).with(channel, 'debate_changed', hash_including(:debate))
     subject.notify_about_votes(debate)
   end
 
   it 'notifies about opening of debate' do
-    expect(broadcaster)
-        .to receive(:push).with(channel, 'debate_opened', {})
+    expect(broadcaster).to receive(:push).with(channel, 'debate_opened', {})
     subject.notify_about_opening(debate)
   end
 
   it 'notifies about closing of debate' do
-    expect(broadcaster)
-        .to receive(:push).with(channel, 'debate_closed', {})
+    expect(broadcaster).to receive(:push).with(channel, 'debate_closed', {})
     subject.notify_about_closing(debate)
   end
 
@@ -28,17 +25,12 @@ describe DebateNotifier do
     let(:vote_change) { instance_double('VoteChange') }
     let(:changes) { {vote_change: {positive: 1, negative: 0}} }
 
-    before do
-      allow(vote_change)
-        .to receive(:to_h).and_return(changes)
-    end
+    before { allow(vote_change).to receive(:to_h).and_return(changes) }
 
     it 'notifies about vote change' do
-      expect(broadcaster)
-        .to receive(:push).with(channel, 'debate_changed', hash_including(:debate, changes))
+      expect(broadcaster).to receive(:push).with(channel, 'debate_changed', hash_including(:debate, changes))
 
       subject.notify_about_votes(debate, vote_change)
     end
   end
-
 end
