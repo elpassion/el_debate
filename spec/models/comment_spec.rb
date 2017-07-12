b84f682b3265c6630f68b28b700db6f5e6c6f0ca
Grzegorz Kemski <gregkem@gmail.com>
2017-07-12 18:40:38 +0200

    Refactor user params delegation spec for a comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,23 +1,27 @@
 require 'rails_helper'
 
 describe Comment do
-  let(:comment) { create(:comment, user: user) }
-  
-  context 'when comment is made by a mobile user' do
-    let(:user) { create(:mobile_user) }
+  context 'user params delegation' do
+    let(:comment) { create(:comment, user: user) }
 
-    it 'delegates user params' do
-      expect(comment.user_image_url).to eq user.image_url
-      expect(comment.user_name).to eq user.name
+    context 'when comment is made by a mobile user' do
+      let(:user) { create(:mobile_user) }
+
+      it 'delegates mobile user params' do
+        expect(comment.user).to be_a MobileUser
+        expect(comment.user_image_url).to eq user.image_url
+        expect(comment.user_name).to eq user.name
+      end
     end
-  end
 
-  context 'when comment is made by a slack user' do
-    let(:user) { create(:slack_user) }
+    context 'when comment is made by a slack user' do
+      let(:user) { create(:slack_user) }
 
-    it 'delegates user params' do
-      expect(comment.user_image_url).to eq user.image_url
-      expect(comment.user_name).to eq user.name
+      it 'delegates slack user params' do
+        expect(comment.user).to be_a SlackUser
+        expect(comment.user_image_url).to eq user.image_url
+        expect(comment.user_name).to eq user.name
+      end
     end
   end
 end
