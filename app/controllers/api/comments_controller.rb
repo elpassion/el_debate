dafc3f07d08a88dcc54288bd65a438740b32fc44
Cezary Karas <cezary.karas@elpassion.pl>
2017-07-26 14:54:48 +0200

    Update edge API as appropriate

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,5 +1,6 @@
 class Api::CommentsController < Api::ApplicationController
   before_action :set_mobile_user
+  before_action :update_first_and_last_name, unless: :first_and_last_name_present?
 
   def create
     if current_debate
@@ -19,5 +20,16 @@ class Api::CommentsController < Api::ApplicationController
 
   def comment_params
     { content: params.fetch(:text) }
+  end
+
+  def update_first_and_last_name
+    @mobile_user.update_attributes(
+      first_name: params.dig(:first_name),
+      last_name:  params.dig(:last_name)
+    )
+  end
+
+  def first_and_last_name_present?
+    @mobile_user.first_name.present? && @mobile_user.last_name.present?
   end
 end
--- app/controllers/api/edge/comments_controller.rb
+++ app/controllers/api/edge/comments_controller.rb
@@ -1,20 +1,10 @@
 class Api::Edge::CommentsController < Api::CommentsController
-  before_action :update_first_and_last_name, unless: :first_and_last_name_present?
-
   private
-
-  def set_mobile_user
-    @mobile_user = MobileUser.find_by(auth_token: @auth_token)
-  end
 
   def update_first_and_last_name
     @mobile_user.update_attributes(
       first_name: params.fetch(:first_name),
       last_name:  params.fetch(:last_name)
     )
-  end
-
-  def first_and_last_name_present?
-    @mobile_user.first_name.present? && @mobile_user.last_name.present?
   end
 end
