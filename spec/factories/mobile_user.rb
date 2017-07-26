10d71de8b384e3dd1f8bc833e1ee600cefffd1a1
Cezary Karas <cezary.karas@elpassion.pl>
2017-07-26 12:03:05 +0200

    Remove unused trait from mobile_user

--- spec/factories/mobile_user.rb
+++ spec/factories/mobile_user.rb
@@ -1,9 +1,5 @@
 FactoryGirl.define do
   factory :mobile_user do
     auth_token
-    trait :with_first_name_and_last_name do
-      sequence(:first_name) { |n| "First Name#{n}" }
-      sequence(:last_name) { |n| "Last Name#{n}" }
-    end
   end
 end
