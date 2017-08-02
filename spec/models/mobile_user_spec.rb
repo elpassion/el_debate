3a33b2cc34b5910e87b53696d4d1847001a51872
Cezary Karas <cezary.karas@elpassion.pl>
2017-08-02 09:36:59 +0200

    Fix dash issue in last_name

--- spec/models/mobile_user_spec.rb
+++ spec/models/mobile_user_spec.rb
@@ -1,22 +1,24 @@
 require 'rails_helper'
 
-describe MobileUser, type: :model do
+describe MobileUser do
 
-  context 'when last_name is short' do
-    let(:mobile_user) { create(:mobile_user, first_name: 'John', last_name: 'Doe') }
+  context 'identity params' do
+    context 'when last_name is short' do
+      let(:mobile_user) { create(:mobile_user, first_name: 'John', last_name: 'Doe') }
 
-    it 'checks users valid initials and full name' do
-      expect(mobile_user.full_name).to eq('John Doe')
-      expect(mobile_user.initials).to eq('JD')
+      it 'checks users valid initials and full name' do
+        expect(mobile_user.full_name).to eq('John Doe')
+        expect(mobile_user.initials).to eq('JD')
+      end
     end
-  end
 
-  context 'when last_name is long' do
-    let(:mobile_user) { create(:mobile_user, first_name: 'John', last_name: 'Last-LastName') }
+    context 'when last_name is long' do
+      let(:mobile_user) { create(:mobile_user, first_name: 'John', last_name: 'Last-LastName') }
 
-    it 'checks users valid initials and full name' do
-      expect(mobile_user.full_name).to eq('John Last-lastname')
-      expect(mobile_user.initials).to eq('JL')
+      it 'checks users valid initials and full name' do
+        expect(mobile_user.full_name).to eq('John Last-Lastname')
+        expect(mobile_user.initials).to eq('JL')
+      end
     end
   end
 end
