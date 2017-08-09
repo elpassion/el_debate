4592b5af8b09a8e1dd3e9d95e16d88f55cd98e44
Cezary Karas <cezary.karas@elpassion.pl>
2017-08-09 15:22:34 +0200

    Refactor comments_controller_spec

--- spec/controllers/api/comments_controller_spec.rb
+++ spec/controllers/api/comments_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe Api::CommentsController do
-
+  let(:params) { { text: 'comment_text', username: 'username' } }
   before(:all) do
     Timecop.freeze(Time.local(2017, 8, 5, 0, 0, 0))
   end
@@ -9,16 +9,16 @@ describe Api::CommentsController do
   after do
     Timecop.return
   end
-  describe '#create' do
-    let(:params) { { text: 'comment_text', username: 'username' } }
 
-    context 'when correct auth_token was given' do
-      let(:debate) { create(:debate) }
-      let(:auth_token) { debate.auth_tokens.create }
-      let!(:mobile_user) { create(:mobile_user, auth_token: auth_token) }
-      before do
-        request.env['HTTP_AUTHORIZATION'] = auth_token.value
-      end
+  context 'when correct auth_token was given' do
+    let(:debate) { create(:debate) }
+    let(:auth_token) { debate.auth_tokens.create }
+    let!(:mobile_user) { create(:mobile_user, auth_token: auth_token, first_name: 'John', last_name: 'Doe') }
+
+    before do
+      request.env['HTTP_AUTHORIZATION'] = auth_token.value
+    end
+    describe '#create' do
 
       it 'executes the comment maker service' do
         expect(CommentMaker).to receive(:perform).with(
@@ -33,35 +33,19 @@ describe Api::CommentsController do
       end
     end
 
-    context 'when auth_token was not given' do
-      it 'does not execute any of the services' do
-        expect(CommentMaker).not_to receive(:perform)
-
-        post :create, params: params
-
-        expect(response.status).to eq(401)
-      end
-    end
-  end
-
-  describe '#index' do
-    context 'when correct auth_token was given' do
-      let(:debate) { create(:debate) }
-      let(:auth_token) { debate.auth_tokens.create }
-      let!(:mobile_user) { create(:mobile_user, auth_token: auth_token, first_name: 'John', last_name: 'Doe') }
-
+    describe '#index' do
       context 'when comments are created' do
         let(:expected_hash) do
-          [{ :content => "content comment 1",
-             :full_name => 'John Doe',
-             :created_at => Time.now.to_i,
-             :user_initials_background_color => mobile_user.initials_background_color,
-             :user_initials => 'JD' },
-           { :content => "content comment 2",
-             :full_name => 'John Doe',
-             :created_at => Time.now.to_i,
-             :user_initials_background_color => mobile_user.initials_background_color,
-             :user_initials => 'JD' }]
+          [{ "content" => "content comment 1",
+             "full_name" => 'John Doe',
+             "created_at" => Time.now.to_i,
+             "user_initials_background_color" => mobile_user.initials_background_color,
+             "user_initials" => 'JD' },
+           { "content" => "content comment 2",
+             "full_name" => 'John Doe',
+             "created_at" => Time.now.to_i,
+             "user_initials_background_color" => mobile_user.initials_background_color,
+             "user_initials" => 'JD' }]
         end
 
         before do
@@ -73,20 +57,27 @@ describe Api::CommentsController do
         it 'retrieve valid status' do
           get :index
           expect(response.status).to eq(200)
-          expect(JSON.parse(response.body, { :symbolize_names => true })).to eq(expected_hash)
+          expect(JSON.parse(response.body)).to eq(expected_hash)
         end
       end
 
       context 'when comments are not created' do
-        before do
-          request.env['HTTP_AUTHORIZATION'] = auth_token.value
-        end
         it 'retrieve valid status' do
           get :index
           expect(response.status).to eq(200)
-          expect(JSON.parse(response.body, { :symbolize_names => true })).to eq([])
+          expect(JSON.parse(response.body)).to eq([])
         end
       end
+    end
+  end
+
+  context 'when auth_token was not given' do
+    it 'does not execute any of the services' do
+      expect(CommentMaker).not_to receive(:perform)
+
+      post :create, params: params
+
+      expect(response.status).to eq(401)
     end
   end
 end
