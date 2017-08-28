2d8e8ffa1b6ac13cb259040221bba13def4d5a9c
Arkadiusz Oleksy <oleksy.arkadiusz@gmail.com>
2017-08-28 14:43:45 +0200

    Refactor mobile user spec

--- spec/models/mobile_user_spec.rb
+++ spec/models/mobile_user_spec.rb
@@ -1,24 +1,42 @@
 require 'rails_helper'
 
 describe MobileUser do
+  subject { mobile_user }
+  let(:mobile_user) { create(:mobile_user, user_attributes) }
+  let(:user_attributes) { {} }
 
-  context 'identity params' do
-    context 'when last_name is short' do
-      let(:mobile_user) { create(:mobile_user, first_name: 'John', last_name: 'Doe') }
+  describe '#full_name' do
+    subject { mobile_user.full_name }
+    let(:user_attributes) { { first_name: first_name, last_name: last_name } }
+    let(:first_name) { FFaker::Name.first_name }
+    let(:last_name) { FFaker::Name.last_name }
 
-      it 'checks users valid initials and full name' do
-        expect(mobile_user.full_name).to eq('John Doe')
-        expect(mobile_user.initials).to eq('JD')
-      end
+    it 'joins first name with last name' do
+      is_expected.to eq("#{first_name} #{last_name}")
     end
 
-    context 'when last_name is long' do
-      let(:mobile_user) { create(:mobile_user, first_name: 'John', last_name: 'last-LastName') }
+    context 'when firstname or lastname is lowercase' do
+      let(:first_name) { 'firstName' }
+      let(:last_name) { 'lastName' }
 
-      it 'checks users valid initials and full name' do
-        expect(mobile_user.full_name).to eq('John Last-LastName')
-        expect(mobile_user.initials).to eq('JL')
+      it 'upcases firstname' do
+        is_expected.to include('FirstName')
       end
+
+      it 'upcases lastname' do
+        is_expected.to include('LastName')
+      end
+    end
+  end
+
+  describe '#initials' do
+    subject { mobile_user.initials }
+    let(:user_attributes) { { first_name: first_name, last_name: last_name } }
+    let(:first_name) { FFaker::Name.first_name }
+    let(:last_name) { FFaker::Name.last_name }
+
+    it 'consist of first letters of first_name and last_name' do
+      is_expected.to eq(first_name[0] + last_name[0])
     end
   end
 end
