61f12d59a029726a7e3f8c2d949044a81e149701
Arkadiusz Oleksy <oleksy.arkadiusz@gmail.com>
2017-08-28 14:45:16 +0200

    refactor mobile user initials to have 2 letters at most

--- spec/models/mobile_user_spec.rb
+++ spec/models/mobile_user_spec.rb
@@ -38,5 +38,50 @@ describe MobileUser do
     it 'consist of first letters of first_name and last_name' do
       is_expected.to eq(first_name[0] + last_name[0])
     end
+
+    context 'when first_name consist of two parts' do
+      let(:first_part) { FFaker::Name.first_name }
+      let(:second_part) { FFaker::Name.first_name }
+      let(:first_name) { [first_part, second_part].join(' ') }
+
+      it 'consist of first letter of first_name first part and first letter of last_name' do
+        is_expected.to eq(first_part[0] + last_name[0])
+      end
+    end
+
+    context 'when last_name consist of two parts' do
+      let(:first_part) { FFaker::Name.first_name }
+      let(:second_part) { FFaker::Name.first_name }
+      let(:last_name) { [first_part, second_part].join(' ') }
+
+      it 'consist of first letter of first_name and first letter of last_name first part' do
+        is_expected.to eq(first_name[0] + first_part[0])
+      end
+    end
+
+    context 'when first_name or last_name is lowercase' do
+      let(:first_name) { 'firstName' }
+      let(:last_name) { 'lastName' }
+
+      it 'upcases initials' do
+        is_expected.to eq('FL')
+      end
+    end
+
+    context 'when last_name is missing' do
+      let(:last_name) { nil }
+
+      it 'consist of only first_name initial letter' do
+        is_expected.to eq(first_name[0])
+      end
+    end
+
+    context 'when first_name is missing' do
+      let(:first_name) { nil }
+
+      it 'consist of only last_name initial letter' do
+        is_expected.to eq(last_name[0])
+      end
+    end
   end
 end
