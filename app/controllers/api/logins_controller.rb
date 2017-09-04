4a1a4e24e97f1444d7ad68d9b012cf57f0d62feb
Cezary Karas <cezary.karas@elpassion.pl>
2017-09-04 11:43:57 +0200

    Refactor logins controller

--- app/controllers/api/logins_controller.rb
+++ app/controllers/api/logins_controller.rb
@@ -3,28 +3,14 @@ class Api::LoginsController < Api::ApplicationController
   before_action :create_debate_auth_token, :set_user, :require_current_debate_not_closed
 
   def create
-    if @user.save
-      render json: { user_id: @user.id, auth_token: @auth_token.value, debate_closed: @debate.closed? }
-    else
-      render json: { error: 'User invalid' }, status: :bad_request
-    end
+    user = Debates::AccessService.new(debate: current_debate).login
+    render json: AuthSerializer.new(user, current_debate)
   end
 
   private
 
-  def create_debate_auth_token
-    @debate = Debate.find_by code: params[:code]
-    return render json: { error: 'Debate not found' }, status: :not_found unless @debate
-    @auth_token = @debate.auth_tokens.create!
-  end
-
-  def set_user
-    @user = User.new(user_params)
-  end
-
-  def user_params
-    {
-      auth_token: @auth_token
-    }
+  def current_debate
+    return @_current_debate if defined?(@_current_debate)
+    @_current_debate = Debate.find_by code: params[:code]
   end
 end
