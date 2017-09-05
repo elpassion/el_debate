868f7edba34bf5f259efceb20934825d2437ac9d
Cezary Karas <cezary.karas@elpassion.pl>
2017-09-05 14:13:45 +0200

    Refactor debates controller spec

--- spec/controllers/api/debates_controller_spec.rb
+++ spec/controllers/api/debates_controller_spec.rb
@@ -1,36 +1,41 @@
 require 'rails_helper'
 
 describe Api::DebatesController do
+  let(:debate) { create(:debate) }
+  let(:auth_token) { debate.auth_tokens.create }
+
   before do
     request.env['HTTP_AUTHORIZATION'] = auth_token.value
   end
 
-  context 'when debate is opened' do
-    let(:debate) { create(:debate) }
-    let(:auth_token) { debate.auth_tokens.create }
-
-    it 'returns valid debates hash' do
+  describe '#show' do
+    subject do
       get :show
-      json_response = JSON.parse(response.body)
+      response
+    end
 
-      expect(response.status).to eq(200)
-      expect(json_response).to include("topic")
-      expect(json_response).to include("answers")
-      expect(json_response).to include("last_answer_id")
+    let(:json_response) { JSON.parse(subject.body) }
+
+    context 'when debate is opened' do
+      it 'returns valid debates hash' do
+        expect(subject).to have_http_status(:ok)
+        expect(json_response).to include("topic" => debate.topic)
+        expect(json_response).to include("answers" => hash_including('positive',
+                                                                     'negative',
+                                                                     'neutral'))
+        expect(json_response).to include("last_answer_id")
+      end
+    end
+
+    context 'when debate is closed' do
+      let(:debate) { create(:debate, :closed_debate) }
+
+      it 'returns valid debate closed error hash' do
+        expect(subject).to have_http_status(:not_acceptable)
+        expect(json_response).to include('error' => 'Debate is closed')
+      end
     end
   end
 
-  context 'when debate is closed' do
-    let(:debate) { create(:debate, :closed_debate) }
-    let(:auth_token) { debate.auth_tokens.create }
 
-    it 'returns valid debate closed error hash' do
-      get :show
-      json_response = JSON.parse(response.body)
-
-      expect(response).to have_http_status(:not_acceptable)
-      expect(json_response).to include('error')
-      expect(json_response['error']).to eq('Debate is closed')
-    end
-  end
 end
