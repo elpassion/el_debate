e7271133c27860b321e5d569acd31804d6782aac
cezarykaras <cezary.karas@elpassion.pl>
2017-09-11 14:16:39 +0200

    Create rejected comments channel (#75)
    * Create rejected comments channel
    
    * Encapsulate sending notifications methods
    
    * Remove unneded space

--- app/services/comment_notifier.rb
+++ app/services/comment_notifier.rb
@@ -4,23 +4,35 @@ class CommentNotifier
     @serializer = serializer
   end
 
-  def send_comment(comment, channel)
+  def comment_added(comment:, channel:)
+    send_comment(comment: comment, channel: channel)
+  end
+
+  def comments_added(comments:, channel:)
+    send_comments(comments: comments, channel: channel)
+  end
+
+  def comment_rejected(comment:, channel:)
+    send_comment(comment: comment, channel: channel, event: 'comment_rejected')
+  end
+
+  private
+
+  def send_comment(comment:, channel:, event: 'comment_added')
     @broadcaster.push(
       channel,
-      "comment_added",
+      event,
       serialize_comment(comment)
     )
   end
 
-  def send_comments(comments, channel)
+  def send_comments(comments:, channel:, event: 'comments_added')
     @broadcaster.push(
       channel,
-      "comments_added",
+      event,
       comments.map { |comment| serialize_comment(comment) }
     )
   end
-
-  private
 
   def serialize_comment(comment)
     @serializer.serialize(comment)
