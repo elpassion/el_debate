a21f66f5ca8800c92542084c0c48f91ec9fd13d7
cezarykaras <cezary.karas@elpassion.pl>
2018-09-07 13:31:21 +0200

    Fix dependencies and migrations (#79)

--- db/migrate/20170718143227_add_first_name_last_name_to_mobile_users.rb
+++ db/migrate/20170718143227_add_first_name_last_name_to_mobile_users.rb
@@ -1,6 +1,6 @@
 class AddFirstNameLastNameToMobileUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column MobileUser.table_name, :first_name, :string
-    add_column MobileUser.table_name, :last_name, :string
+    add_column :mobile_users, :first_name, :string
+    add_column :mobile_users, :last_name, :string
   end
 end
--- db/migrate/20170725080313_add_avatar_color_to_users.rb
+++ db/migrate/20170725080313_add_avatar_color_to_users.rb
@@ -1,5 +1,5 @@
 class AddAvatarColorToUsers < ActiveRecord::Migration[5.0]
   def change
-    add_column MobileUser.table_name, :avatar_color, :string
+    add_column :mobile_users, :avatar_color, :string
   end
 end
